f5d516b1722b4cf63781d5ca1b194fa991851c2e
Kieran Thomas <kthom796@gmail.com>
2020-09-18 13:15:02 +1000

    sewm

--- sewm.m
+++ sewm.m
@@ -1,4 +1,4 @@
-function [SEMw, Mw] = sewm(x,w)
+function [SEMw, Mw] = sewm(x,w,dimension)
 % Standard error of a weighted mean
 %
 % I N P U T
@@ -21,11 +21,14 @@ function [SEMw, Mw] = sewm(x,w)
 % DKS
 % 2019-02-23
 
+if nargin<3
+    dimension = 1;
+end
 
 n = length(x);      % number of samples
-w_sum = sum(w);     
+w_sum = nansum(w,dimension);     
 w_sqrd = w.^2;      
-Mw = sum(w.*x)./w_sum;     
+Mw = nansum(w.*x,dimension)./w_sum;     
 % w_mean = mean(w);   % mean of weights
 
 % % formula as in Gatz and Smith
@@ -34,8 +37,8 @@ Mw = sum(w.*x)./w_sum;
 %     + Mw^2 * sum( (w - w_mean).^2 ) ) );
 
 % formula in a computationally friendly form [stackexchange forum]
-SEMw = sqrt( n./((n-1).*w_sum.^2) .* ( sum( (w_sqrd.*x.^2) ) ...
-                            - 2.*Mw.*sum( w_sqrd.*x ) ...
-                            + Mw.^2.*sum( w_sqrd ) ) );
+SEMw = sqrt( n./((n-1).*w_sum.^2) .* ( nansum( (w_sqrd.*x.^2), dimension ) ...
+                            - 2.*Mw.*nansum( w_sqrd.*x, dimension ) ...
+                            + Mw.^2.*nansum( w_sqrd, dimension ) ) );
 
 end
